d731337afbd87f399eaa9186c51e2ac86047ed28
itwill <itwill@DESKTOP-T29ORBI>
2023-03-29 15:10:21 +0900

    Day 1

---
+++ java_1/src/basic/DataTypeApp.java
@@ -0,0 +1,15 @@
+package basic;
+
+// 자료형(DataType) : 값을 표현하기 위한 키보드 또는 특정 대상을 표현하기 위한 식별자
+// => 값이나 특정 대상을 저장하기 위한 변수를 선언하기 위해 사용
+// 1. 원시형(PrimitiviType) : 값을 표현하기 위한 자료형 -키워드
+// => 숫자형(정수형, 실수형, 문자형) -연산 가능, 논리형
+// 2. 참조형(ReferenceType) : 특정 대상을 표현하기 위한 자료형 - 식별자(클래스)
+// => String 클래스 : 문자열을 표현하기 위한 자료형(클래스)
+
+public class DataTypeApp {
+	public static void main(String[] args) {
+		
+	}
+
+}
