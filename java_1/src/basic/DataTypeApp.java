c9c2b1e4276772ab3382bd4bd88f42d21552b24b
itwill <itwill@DESKTOP-T29ORBI>
2023-03-29 15:12:25 +0900

    Day 1

--- java_1/src/basic/DataTypeApp.java
+++ java_1/src/basic/DataTypeApp.java
@@ -7,9 +7,11 @@ package basic;
 // 2. 참조형(ReferenceType) : 특정 대상을 표현하기 위한 자료형 - 식별자(클래스)
 // => String 클래스 : 문자열을 표현하기 위한 자료형(클래스)
 
+// 리터럴(Literal) : 프로그램에서 값을 표현하여 사용하는 방식
+
 public class DataTypeApp {
 	public static void main(String[] args) {
-		
+		// 정수형 (IntegerType) 리터럴 : 소숫점이 없는 숫자값
 	}
 
 }
