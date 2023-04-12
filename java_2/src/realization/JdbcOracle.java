30301ee572e39beec60a29e267e86baf1b2fcd19
itwill <itwill@DESKTOP-T29ORBI>
2023-04-12 20:39:29 +0900

    .

--- java_2/src/realization/JdbcOracle.java
+++ java_2/src/realization/JdbcOracle.java
@@ -2,7 +2,8 @@ package realization;
 
 public class JdbcOracle implements Jdbc {
 	/*
-	 * public void add() { System.out.println("[oracle] 학생정보를 삽입하는 기능을 제공하는 메소드"); }
+	 * public void add() { 
+	 * System.out.println("[oracle] 학생정보를 삽입하는 기능을 제공하는 메소드"); }
 	 * 
 	 * public void modify() {
 	 * System.out.println("[oracle] 학생정보를 변경하는 기능을 제공하는 메소드"); }
